33236692feb5eb7cc65a0727a8679d7b0b9be5d9
EricMungai97 <mungaikinus@gmail.com>
2023-04-11 00:53:18 -0700

    image fix

--- src/pages/carousel.js
+++ src/pages/carousel.js
@@ -4,20 +4,20 @@ import Carousel from "react-bootstrap/Carousel";
 
 function Caarousel() {
     const images = [
-      { src: "/image/pf (1).jpg" },
-      { src: "/image/pf (2).jpg"},
-      { src: "/image/pf (3).jpg" },
-      { src: "/image/pf (4).jpg" },
-      { src: "/image/pf (5).jpg" },
-      { src: "/image/pf (6).jpg" },
-      { src: "/image/pf (7).jpg" },
+      { src: "/image/pf (1).jpg", alt: "", width: 400, height: 300 },
+      { src: "/image/pf (2).jpg", alt: "", width: 900, height: 300 },
+      { src: "/image/pf (3).jpg", alt: "", width: 400, height: 300 },
+      { src: "/image/pf (4).jpg", alt: "", width: 400, height: 300 },
+      { src: "/image/pf (5).jpg", alt: "", width: 400, height: 300 },
+      { src: "/image/pf (6).jpg", alt: "", width: 400, height: 300 },
+      { src: "/image/pf (7).jpg", alt: "", width: 400, height: 300 },
     ];
   
     return (
       <Carousel>
         {images.map((image, index) => (
           <Carousel.Item key={index}>
-            <img src={image.src}/>
+            <Image src={image.src} alt={image.alt} width={image.width} height={image.height} />
           </Carousel.Item>
         ))}
       </Carousel>
