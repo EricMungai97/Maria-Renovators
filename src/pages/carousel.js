87d4fd21f4462af1fc4e52c584ec1706db612a67
EricMungai97 <mungaikinus@gmail.com>
2023-04-29 13:36:39 -0700

    fixed carousel

--- src/pages/carousel.js
+++ src/pages/carousel.js
@@ -3,25 +3,34 @@ import Image from "next/image";
 import Carousel from "react-bootstrap/Carousel";
 
 function Caarousel() {
-    const images = [
-      { src: "/image/pf (1).jpg", alt: "", width: 900, height: 300 },
-      { src: "/image/pf (2).jpg", alt: "", width: 900, height: 300 },
-      { src: "/image/pf (3).jpg", alt: "", width: 400, height: 300 },
-      { src: "/image/pf (4).jpg", alt: "", width: 400, height: 300 },
-      { src: "/image/pf (5).jpg", alt: "", width: 400, height: 300 },
-      { src: "/image/pf (6).jpg", alt: "", width: 400, height: 300 },
-      { src: "/image/pf (7).jpg", alt: "", width: 400, height: 300 },
-    ];
-  
-    return (
-      <Carousel>
-        {images.map((image, index) => (
-          <Carousel.Item key={index}>
-            <Image src={image.src} alt={image.alt} width={image.width} height={image.height} />
-          </Carousel.Item>
-        ))}
-      </Carousel>
-    );
+  const images = [
+    { src: "/image/image1.JPEG", alt: ""},
+    { src: "/image/image2.JPEG", alt: ""},
+    { src: "/image/image3.JPEG", alt: ""},
+    { src: "/image/image4.JPEG", alt: ""},
+    { src: "/image/image5.JPEG", alt: ""},
+    { src: "/image/image7.JPEG", alt: ""},
+    { src: "/image/image8.JPEG", alt: ""},
+    { src: "/image/image9.JPEG", alt: ""},
+  ];
+
+  return (
+    <Carousel>
+      {images.map((image, index) => (
+        <Carousel.Item key={index}>
+          <div className="h-screen w-screen">
+            <Image
+              src={image.src}
+              alt={image.alt}
+              height={image.height}
+              objectFit="cover"
+              layout="fill"
+            />
+          </div>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
 }
 
 export default Caarousel;
